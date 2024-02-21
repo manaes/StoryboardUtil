c6617c471b06a42c058d8804ee0149f5648477bf
wanny.park <wanny.park@pandora.tv>
2024-02-21 17:29:41 +0900

    edit package

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
         .target(
             name: "StoryboardUtil",
             dependencies: [],
+            path: "StoryboardUtil",
             resources: [.copy("StoryboardList.txt")]),
     ]
 )
